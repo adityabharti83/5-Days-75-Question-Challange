89fea72e07044199b8c5e6d0a7aaf1ed3c2ba9a7
Aditya Bharti <135196678+adityabharti83@users.noreply.github.com>
2023-09-30 20:35:54 +0530

    Delete 3 Program to Multiply Two Floating-Point Numbers.c

--- 3 Program to Multiply Two Floating-Point Numbers.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    double a, b, product;
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &a, &b);  
-    product = a * b;
-    printf("Product = %.2lf", product);
-    return 0;
-}
